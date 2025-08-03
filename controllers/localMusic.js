4de89c34eab3e4c1e8eb9b6bdbb4d4d0bfa74e55
amalsodharan <amalsodharan789@gmail.com>
2025-08-03 12:08:00 +0530

    local music adding functionality

--- controllers/localMusic.js
+++ controllers/localMusic.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const addMusic = async (req, res) => {
     const reqInput = ["name", "artist", "album", "duration"];
@@ -11,21 +15,23 @@ const addMusic = async (req, res) => {
         }
     })
     console.log(reqInput)
-    const folderPath = path.join(__dirname, 'cdn/assets');
-     if (!fs.existsSync(folderPath)) {
+    const folderPath = path.join(__dirname, `../cdn/assets/${input.name}`);
+
+    if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
     }
     const imageFile = req.files.find(file => file.fieldname === 'image');
     const audioFile = req.files.find(file => file.fieldname === 'audio');
-    if(imageFile === undefined || imageFile === ""){
-        return res.status(401).json({message : `Image is required`});
+    if (!imageFile || !imageFile.buffer || !audioFile || !audioFile.buffer) {
+        return res.status(401).json({message : `Desired files are missing`});
     }else{
-        let filePath = path.join(folderPath, imageFile.originalname);
-        fs.writeFileSync(filePath, file.buffer);
+        const imageExt = path.extname(imageFile.originalname);
+        const audioExt = path.extname(audioFile.originalname);
+        const imagePath = path.join(folderPath, `image${imageExt}`);
+        fs.writeFileSync(imagePath, imageFile.buffer);
+        const audioPath = path.join(folderPath, `audio${audioExt}`);
+        fs.writeFileSync(audioPath, audioFile.buffer);
     }
-    // if(audioFile === undefined || audioFile === ""){
-    //     return res.status(401).json({message : `Audio file is required`});
-    // }
     res.status(200).json({message : 'Data saved successfully'})
 }
 
